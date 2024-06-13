d78607be7119aad30e266bd47ad49859c8aad103
Akin-O <148026745+Akin-O@users.noreply.github.com>
2024-06-13 12:41:40 -0400

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Dashboard from './components/Dashboard'
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="title">
+        C4C: Projects
+        <div>
+          <img src="https://www.c4cneu.com/assets/favicon.ico" />
+        </div>
+      </h1>
+      <Dashboard />
+    </>
+  )
+}
+
+export default App
